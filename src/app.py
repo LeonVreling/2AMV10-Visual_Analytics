beaf251ceaea53f46a0f21271922df712c711137
Leon Vreling <l.m.vreling@student.tue.nl>
2023-03-03 11:26:29 +0100

    Add toy radviz for two songs

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -17,11 +18,11 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="fc126aaa02334aae871ae1
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME])
 
-
+# Last 20 saved tracks of a user
 tracks = sp.current_user_saved_tracks()
 track_options = [{"label": track["track"]["name"], "value": track["track"]["id"]} for track in tracks["items"]]
 
-
+# Top 10 songs of user with graph for tempo and duration
 top_tracks = sp.current_user_top_tracks(limit=10, time_range='short_term')
 top_tracks = [{"title": track["name"], "artist": track["artists"][0]["name"], "id": track["id"]} for track in top_tracks["items"]]
 app.logger.info(top_tracks)
@@ -53,6 +54,50 @@ fig.update_traces(
     ])
 )
 
+
+# RadViz for two songs
+index_first_song = 0
+index_second_song = 1
+
+categories = ["acousticness", "danceability", "energy", "instrumentalness", "liveness", "loudness", "speechiness", "valence"]
+
+# Scale of the different categories, according to https://developer.spotify.com/documentation/web-api/reference/#/operations/get-audio-features
+# acousticness: [0,1]
+# danceability: [0,1]
+# energy: [0,1]
+# instrumentalness: [0,1]
+# liveness: [0,1]
+# loudness: typically [-60,0]
+# speechiness: [0,1]
+# valence: [0,1]
+# --> Need to scale the loudness, using affine transformation
+
+# new_loudness = (original_loudness + 60) * (1 / 60) + 0 = 1 + original_loudness / 60
+
+fig_radviz = go.Figure()
+
+fig_radviz.add_trace(go.Scatterpolar(
+      r=[top_tracks_features[index_first_song][category] if category is not "loudness" else 1+top_tracks_features[index_first_song]["loudness"]/60 for category in categories],
+      theta=categories,
+      fill='toself',
+      name= top_tracks[index_first_song]["title"] + " - " + top_tracks[index_first_song]["artist"]
+))
+fig_radviz.add_trace(go.Scatterpolar(
+      r=[top_tracks_features[index_second_song][category] if category is not "loudness" else 1+top_tracks_features[index_second_song]["loudness"]/60 for category in categories],
+      theta=categories,
+      fill='toself',
+      name= top_tracks[index_second_song]["title"] + " - " + top_tracks[index_second_song]["artist"]
+))
+
+fig_radviz.update_layout(
+  polar=dict(
+    radialaxis=dict(
+      visible=True,
+      range=[0, 1]
+    )),
+  showlegend=True,
+  title="Similarness of two songs"
+)
 
 
 app.layout = dbc.Container([
@@ -102,6 +147,15 @@ app.layout = dbc.Container([
             dcc.Graph(
                 id='example-graph',
                 figure=fig
+            )
+        ])
+    ]),
+
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(
+                id='example-graph',
+                figure=fig_radviz
             )
         ])
     ])
